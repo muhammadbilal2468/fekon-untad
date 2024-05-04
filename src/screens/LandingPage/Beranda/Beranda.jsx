2656bd1d2f63a38fc808d9bdef8aed02a4644fc0
Muhammad Bilal <muhammadbilalll2468@gmail.com>
2024-05-04 11:18:39 +0800

    update

--- src/screens/LandingPage/Beranda/Beranda.jsx
+++ src/screens/LandingPage/Beranda/Beranda.jsx
@@ -1,11 +1,14 @@
 import AOS from "aos";
 import "aos/dist/aos.css";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { bannerVid } from "../../../assets";
 
 // ------------------------- UGM --------------------------
 
 const Beranda = () => {
+  const [prestasiCount, setPrestasiCount] = useState(3);
+  const [beritaCount, setBeritaCount] = useState(8);
+
   useEffect(() => {
     AOS.init({ duration: 1000 });
   }, []);
@@ -42,6 +45,73 @@ const Beranda = () => {
 
         {/* ------------ ----------------- */}
         <div className="pt-[100vh]"></div>
+
+        {/* ------------------------------ */}
+        <div className="mb-10">
+          <h2 className="text-right text-primary text-2xl font-bold mb-2">
+            Prestasi
+          </h2>
+          <div className="flex xl:flex-row flex-col gap-10 xl:h-96 h-fit">
+            <div className="relative w-7/12 h-full">
+              <img
+                src="https://source.unsplash.com/random"
+                alt=""
+                className="absolute w-full bg-cover rounded-xl h-full"
+              />
+            </div>
+            <div className="flex flex-col justify-between xl:gap-2 gap-4 xl:w-5/12 w-full">
+              {Array.from({ length: prestasiCount }).map((_, index) => (
+                <div key={index} className="flex gap-2 h-1/3">
+                  <img
+                    src="https://source.unsplash.com/random"
+                    alt=""
+                    className="xl:w-36 w-64 bg-cover rounded-xl xl:h-full h-32"
+                  />
+                  <div className="flex flex-col gap-3">
+                    <div className="flex xl:text-xs text-[8px] gap-1 justify-between">
+                      <p className="font-bold">Aura Dwi Kartika</p>
+                      <p className="">2 Desember 2024</p>
+                    </div>
+                    <p className="font-bold xl:text-sm text-[10px]">
+                      The 5th International Public Sector Conference (IPSC) 2023
+                    </p>
+                    <p className="text-gray-500 text-xs text-[9px] text-justify">
+                      International Public Sector Conference (IPSC) Ke-5 2023
+                      diselenggarakan oleh Fakultas Ekonomi dan Bisnis
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <h2 className="text-primary text-2xl font-bold mt-10 mb-2">
+            Berita Terbaru
+          </h2>
+          <div className="grid xl:grid-cols-4 grid-cols-2 gri gap-4">
+            {Array.from({ length: beritaCount }).map((_, index) => (
+              <div className="flex flex-col gap-2">
+                <img
+                  src="https://source.unsplash.com/random"
+                  alt=""
+                  className="w-full h-36 bg-cover rounded-xl"
+                />
+                <div className="flex flex-col gap-2">
+                  <div className="flex xl:text-xs text-[8px] gap-1 justify-between">
+                    <p className="font-bold">Aura Dwi Kartika</p>
+                    <p className="">2 Desember 2024</p>
+                  </div>
+                  <p className="font-bold xl:text-sm text-[10px]">
+                    The 5th International Public Sector Conference (IPSC) 2023
+                  </p>
+                  <p className="text-gray-500 text-xs text-[9px] text-justify">
+                    International Public Sector Conference (IPSC) Ke-5 2023
+                    diselenggarakan oleh Fakultas Ekonomi dan Bisnis
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
